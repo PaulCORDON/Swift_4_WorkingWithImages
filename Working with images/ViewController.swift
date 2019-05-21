fd0cd967268d21762aa579e7b43b95c4f7ce5f23
Paul <paul.cordon@laposte.net>
2019-05-21 10:26:06 +0200

    init

--- Working with images/ViewController.swift
+++ Working with images/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imgView: UIImageView!
 
+    var myInteger = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func ChangePictureBtnClick(_ sender: Any) {
 
+//        if imgView.image == UIImage(named: "XR.jpg"){
+//            imgView.image = UIImage(named: "XS.jpeg")
+//        }
+//        else{
+//            imgView.image = UIImage(named: "XR.jpg")
+//        }
+        
+        if(myInteger==0){
+            imgView.image = UIImage(named: "XR.jpg")
+            myInteger += 1
+        }else if(myInteger==1){
+            imgView.image = UIImage(named: "XS.jpeg")
+            myInteger -= 1
+        }
+        
+    }
+    
 }
 
